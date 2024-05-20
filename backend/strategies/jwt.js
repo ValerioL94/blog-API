8d8706bc878112abaf6d7bc89cf9a8588a8d0af6
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-05-20 13:45:03 +0200

    Add jwt strategy

---
+++ backend/strategies/jwt.js
@@ -0,0 +1,21 @@
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const User = require('../models/user');
+
+const opts = {};
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+opts.secretOrKey = process.env.SECRET;
+
+module.exports = new JwtStrategy(opts, function (jwt_payload, done) {
+  User.findOne({ email: jwt_payload.email })
+    .then((user) => {
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    })
+    .catch((err) => {
+      return done(err, false);
+    });
+});
