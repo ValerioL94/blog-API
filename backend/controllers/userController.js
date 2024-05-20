862728b3e3a279fc72b0d6f691da2ab32dcd69c4
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-05-20 13:44:09 +0200

    Add user sign-up-, log-in, log-out

---
+++ backend/controllers/userController.js
@@ -0,0 +1,85 @@
+const User = require('../models/user');
+
+const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.sign_up = [
+  body('username')
+    .trim()
+    .escape()
+    .isLength({ min: 1 })
+    .withMessage('Username must contain at least 1 character')
+    .custom(async (value) => {
+      const user = await User.findOne({ username: value });
+      if (user) {
+        throw new Error('Username already in use');
+      }
+    }),
+  body('email')
+    .trim()
+    .escape()
+    .isEmail()
+    .withMessage('Enter a valid E-mail')
+    .custom(async (value) => {
+      const user = await User.findOne({ email: value });
+      if (user) {
+        throw new Error('E-mail already in use');
+      }
+    }),
+  body('password')
+    .trim()
+    .escape()
+    .isLength({ min: 8 })
+    .withMessage('Password must contain at least 8 characters'),
+  body('passwordConfirm', 'Passwords do not match').custom((value, { req }) => {
+    return value === req.body.password;
+  }),
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    const newUser = new User({
+      username: req.body.username,
+      email: req.body.email,
+    });
+    if (!errors.isEmpty()) {
+      return res.json({ newUser, errors: errors.array() });
+    }
+    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+      if (err) {
+        return next(err);
+      }
+      newUser.password = hashedPassword;
+      await newUser.save();
+      res.json({ message: 'Sign-up successful' });
+    });
+  }),
+];
+
+exports.log_in = asyncHandler(async (req, res, next) => {
+  let { email, password } = req.body;
+  const user = await User.findOne({ email: email });
+  if (user) {
+    const match = await bcrypt.compare(password, user.password);
+    if (match) {
+      const opts = {};
+      opts.expiresIn = 300;
+      const secret = process.env.SECRET;
+      const token = jwt.sign({ email }, secret, opts);
+      return res.status(200).json({
+        message: 'Auth Passed',
+        token,
+      });
+    }
+  }
+  return res.status(401).json({ message: 'Auth Failed' });
+});
+
+exports.log_out = asyncHandler(async (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.json({ message: 'Log-out successful' });
+  });
+});
