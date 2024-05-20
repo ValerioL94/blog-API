b2005e009185eee81be4dc790ade7c5d65cd4a49
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-05-20 13:42:28 +0200

    Update dependencies

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,15 @@
 const createError = require('http-errors');
 const express = require('express');
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const bcrypt = require('bcryptjs');
-const session = require('express-session');
 const cors = require('cors');
 
-const User = require('./models/user');
-const Post = require('./models/post');
 const mongoDB = process.env.MONGODB_URI;
 const indexRouter = require('./routes/index');
 const blogRouter = require('./routes/blog');
+const jwtStrategy = require('./strategies/jwt');
 
 const app = express();
 
@@ -23,62 +18,14 @@ async function main() {
   await mongoose.connect(mongoDB);
 }
 
-// app.use(
-//   session({
-//     secret: process.env.SECRET,
-//     resave: false,
-//     saveUninitialized: true,
-//   })
-// );
+app.options('*', cors());
 
-app.set('view engine', 'ejs');
+passport.use(jwtStrategy);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.options('*', cors());
-// app.use(passport.session());
-
-// passport.use(
-//   new LocalStrategy(
-//     { usernameField: 'email' },
-//     async (username, password, done) => {
-//       try {
-//         const user = await User.findOne({ email: username });
-//         if (!user) {
-//           return done(null, false, { message: 'Incorrect E-mail' });
-//         }
-//         const match = await bcrypt.compare(password, user.password);
-//         if (!match) {
-//           // passwords do not match!
-//           return done(null, false, { message: 'Incorrect password' });
-//         }
-//         return done(null, user);
-//       } catch (error) {
-//         return done(error);
-//       }
-//     }
-//   )
-// );
-// passport.serializeUser((user, done) => {
-//   done(null, user.id);
-// });
-
-// passport.deserializeUser(async (id, done) => {
-//   try {
-//     const user = await User.findById(id);
-//     done(null, user);
-//   } catch (error) {
-//     done(error);
-//   }
-// });
-
-// app.use((req, res, next) => {
-//   res.locals.currentUser = req.user;
-//   next();
-// });
 
 app.use('/', indexRouter);
 app.use('/blog', blogRouter);
@@ -94,9 +41,8 @@ app.use(function (err, req, res, next) {
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
-  res.render('error', { title: 'Error' });
+  res.json({ error: err });
 });
 
 module.exports = app;
