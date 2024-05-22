53ee33a70d6e622ad3675676d8fe4ebe02dcf57e
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-05-22 13:14:43 +0200

    Add home route and styles

---
+++ frontend-user/src/routes/Home.jsx
@@ -0,0 +1,11 @@
+import '../styles/Home.css';
+
+export default function Home() {
+  return (
+    <div className="home-wrapper">
+      <h1>Homepage</h1>
+      <p>Welcome! Check out the posts and leave a comment if you like.</p>
+      <p>No registration needed at the moment!</p>
+    </div>
+  );
+}
