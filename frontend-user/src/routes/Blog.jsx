935f3c79b70a995e4af04c5f82bbdd9d14143496
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-05-22 18:06:29 +0200

    Refactor blog.jsx to use loaderData

--- frontend-user/src/routes/Blog.jsx
+++ frontend-user/src/routes/Blog.jsx
@@ -1,39 +1,19 @@
-import { useState, useEffect } from 'react';
+import { useLoaderData, Link } from 'react-router-dom';
 import '../styles/Blog.css';
 
 export default function Blog() {
-  const [posts, setPosts] = useState([]);
-  const getRequest = async (url) => {
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error(`HTTP error: Status ${response.status}`);
-    }
-    return response.json();
-  };
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        const posts = await getRequest('http://localhost:3000/blog/posts');
-        setPosts(posts);
-      } catch (error) {
-        setPosts(null);
-        console.log(error);
-      }
-    };
-    fetchPosts();
-  }, []);
+  const { posts } = useLoaderData();
   return (
     <div className="wrapper">
       <h2>Posts</h2>
       <div className="posts-wrapper">
-        {!posts.length && <h2>Fetching posts...</h2>}
-        {posts &&
+        {!posts.length && <h3>Fetching posts...</h3>}
+        {posts.length &&
           posts.map((data) =>
             data.published ? (
               <div key={data._id} className="post-preview">
                 <h3>
-                  <a href={'/posts/' + data._id}>{data.title}</a>
+                  <Link to={`${data._id}`}>{data.title}</Link>
                 </h3>
                 <p>
                   <strong>Author:</strong> {data.author.username}
