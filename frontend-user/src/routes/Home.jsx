24fb45c861b9270c944dc0bfbaef1b90c4674a19
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-05-22 13:50:47 +0200

    Update styles

--- frontend-user/src/routes/Home.jsx
+++ frontend-user/src/routes/Home.jsx
@@ -2,7 +2,7 @@ import '../styles/Home.css';
 
 export default function Home() {
   return (
-    <div className="home-wrapper">
+    <div className="wrapper">
       <h1>Homepage</h1>
       <p>Welcome! Check out the posts and leave a comment if you like.</p>
       <p>No registration needed at the moment!</p>
