c3db122b927762c8af8200326495c8ca19a15ba6
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-05-22 13:50:57 +0200

    Add about page content and styles

---
+++ frontend-user/src/routes/About.jsx
@@ -0,0 +1,26 @@
+import '../styles/About.css';
+
+export default function About() {
+  return (
+    <div className="wrapper">
+      <h1>About</h1>
+      <p>This is the front-end site of the blog-API project.</p>
+      <p>
+        As you might have read on the home page there is currently no
+        registration needed to leave comments;
+      </p>
+      <p>
+        This means that you won&apos;t be able to edit or delete comments, so
+        keep that in mind before posting.
+      </p>
+      <p>
+        If you don&apos;t know why you&apos;re here or what I&apos;m talking
+        about, you might want to check this out at{' '}
+        <a href="https://www.theodinproject.com/lessons/nodejs-blog-api">
+          The Odin Project
+        </a>{' '}
+        website then!
+      </p>
+    </div>
+  );
+}
