d62cf5fd74a14a6349d087d5469f2be3eff68fd1
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-05-22 13:14:58 +0200

    add blog posts route and styles

---
+++ frontend-user/src/routes/Blog.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from 'react';
+import '../styles/Blog.css';
+
+export default function Blog() {
+  const [posts, setPosts] = useState([]);
+  const getRequest = async (url) => {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error: Status ${response.status}`);
+    }
+    return response.json();
+  };
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const posts = await getRequest('http://localhost:3000/blog/posts');
+        setPosts(posts);
+      } catch (error) {
+        setPosts(null);
+        console.log(error);
+      }
+    };
+    fetchPosts();
+  }, []);
+  return (
+    <main>
+      <h2>Posts</h2>
+      <div className="posts-wrapper">
+        {!posts.length && <h2>Fetching posts...</h2>}
+        {posts &&
+          posts.map((data) =>
+            data.published ? (
+              <div key={data._id} className="post-preview">
+                <h3>
+                  <a href={'/posts/' + data._id}>{data.title}</a>
+                </h3>
+                <p>
+                  <strong>Author:</strong> {data.author.username}
+                </p>
+                <p>
+                  <strong>Date:</strong>{' '}
+                  {new Date(data.createdAt).toLocaleString()}
+                </p>
+              </div>
+            ) : (
+              ''
+            )
+          )}
+      </div>
+    </main>
+  );
+}
