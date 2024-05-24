2daf2ff26a80dcc192418bd09c5ed1fff6366221
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-05-24 12:10:06 +0200

    Update home page

---
+++ frontend-author/src/routes/Home.jsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div className="wrapper">
+      <h1>Homepage</h1>
+      <p>
+        Welcome to the homepage! On this site registered authors can manage both
+        posts and comments.
+      </p>
+      <p>
+        Already have an account? Go ahead and log in then! <br /> Don&apos;t
+        have an account? If you know the secret password you can create one in
+        the blink of an eye!{' '}
+      </p>
+    </div>
+  );
+}
