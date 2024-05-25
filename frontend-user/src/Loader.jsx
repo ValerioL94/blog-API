16db4cdd22874538a963438f95f429c1b68409b0
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-05-25 13:11:53 +0200

    Add router action for new comments

--- frontend-user/src/Loader.jsx
+++ frontend-user/src/Loader.jsx
@@ -1,12 +1,19 @@
-import { fetchPosts, fetchPost } from './posts';
+import { fetchPosts, fetchPost, sendComment } from './posts';
 
 export async function postsLoader() {
   const posts = await fetchPosts();
   return { posts };
 }
 
-export async function postLoader(req) {
-  const postData = await fetchPost(req.params.postId);
+export async function postLoader({ params }) {
+  const postData = await fetchPost(params.postId);
   const { commentsInPost, post } = postData;
   return { commentsInPost, post };
+}
+
+export async function commentAction({ params, request }) {
+  const formData = await request.formData();
+  const payload = Object.fromEntries(formData.entries());
+  const response = await sendComment(params.postId, payload);
+  return response;
 }
--- frontend-user/src/posts.js
+++ frontend-user/src/posts.js
@@ -6,6 +6,20 @@ const getRequest = async (url) => {
   return response.json();
 };
 
+const postRequest = async (url, formData) => {
+  const response = await fetch(url, {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(formData),
+  });
+  if (!response.ok) {
+    throw new Error(`HTTP error: Status ${response.status}`);
+  }
+  return response.json();
+};
+
 export async function fetchPosts() {
   try {
     const posts = await getRequest('http://localhost:3000/blog/posts');
@@ -19,6 +33,18 @@ export async function fetchPost(postid) {
   try {
     const post = await getRequest('http://localhost:3000/blog/posts/' + postid);
     return post;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+export async function sendComment(postid, formData) {
+  try {
+    const response = await postRequest(
+      'http://localhost:3000/blog/posts/' + postid + '/comments',
+      formData
+    );
+    return response;
   } catch (error) {
     throw new Error(error.message);
   }
