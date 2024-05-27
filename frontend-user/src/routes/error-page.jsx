b794d1fb18f74764e0a39256806b351cf7c5d6e2
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-05-27 09:47:04 +0200

    Update error pages

--- frontend-user/src/routes/error-page.jsx
+++ frontend-user/src/routes/error-page.jsx
@@ -1,15 +1,34 @@
-import { useRouteError } from 'react-router-dom';
+import { useRouteError, isRouteErrorResponse } from 'react-router-dom';
 import { Link } from 'react-router-dom';
 export default function ErrorPage() {
   const error = useRouteError();
 
+  function RootBoundary() {
+    const error = useRouteError();
+
+    if (isRouteErrorResponse(error)) {
+      if (error.status === 401) {
+        return <p>You aren&apos;t authorized to see this</p>;
+      }
+      if (error.status === 404) {
+        return <p>This page doesn&apos;t exist!</p>;
+      }
+      if (error.status === 418) {
+        return <p>🫖</p>;
+      }
+      if (error.status === 503) {
+        return <p>Looks like our API is down</p>;
+      }
+    }
+    return <p>Something went wrong</p>;
+  }
   return (
     <div id="error-page">
       <h1>Oops!</h1>
-      <p>Looks like something went wrong:</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+      <i>
+        {error.status} {error.statusText || error.message}
+      </i>
+      {RootBoundary()}
       <Link to="home">
         You can go back to the home page by clicking here, though!
       </Link>
