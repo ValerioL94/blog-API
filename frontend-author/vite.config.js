c0b7f59508e419988f4b8d23e422e7df3ced0ccc
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-05-28 17:30:10 +0200

    Fix typo

--- frontend-author/vite.config.js
+++ frontend-author/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/blog': {
+      '/api': {
         target: 'http://localhost:3000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
