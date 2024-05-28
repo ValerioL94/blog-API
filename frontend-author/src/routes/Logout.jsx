d68b1e61e251167d691d7cc8d523218eeef0dd39
ValerioL94 <134592955+ValerioL94@users.noreply.github.com>
2024-05-28 19:20:06 +0200

    Add simple logout

---
+++ frontend-author/src/routes/Logout.jsx
@@ -0,0 +1,21 @@
+import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../provider/context';
+import { useEffect } from 'react';
+export default function Logout() {
+  const { setToken } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setToken();
+      navigate('/home', { replace: true });
+    }, 2000);
+  }, [setToken, navigate]);
+
+  return (
+    <div className="wrapper">
+      <h1>Logout Page</h1>
+      <p>Logging out...</p>
+    </div>
+  );
+}
